2af562303bac35538cc93d0b05791a6eb6f7d73e
Maxim Butenko <maxim.butenko92.1@gmail.com>
2022-06-02 23:03:16 +0300

    fix: import modules

--- components/authentication/register/useRegisterHandler.ts
+++ components/authentication/register/useRegisterHandler.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { axios } from '../../../axios'
 import { SubmitHandler } from '../../form'
-import { SignUpData } from '../common'
+import { SignUpData } from '../content'
 
 export function useRegisterHandler() {
 	const [signUpStatus, setSignUpStatus] = useState<null | 'submitting' | 'success'>(null)
